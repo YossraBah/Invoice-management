0ecf650a0bae203d03fb8594d9cb048b1795037b
Yosra Bahloul <ybahloul@diginov.tech>
2024-02-18 12:21:59 +0100

    finish nextjs learning tutorial

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default function Page() {
     return<p>Customers Page</p>;
   }
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -22,7 +22,6 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname()
-  console.log(pathname)
   return (
     <>
       {links.map((link) => {
