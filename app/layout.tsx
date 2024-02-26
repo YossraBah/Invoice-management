6945245a6aafe9aeba141a7bbcd1fc9c9bae6adf
Yosra Bahloul <ybahloul@diginov.tech>
2024-02-26 16:52:24 +0100

    change title of default meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: {
     template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    default: ' Yo Acme Dashboard',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
