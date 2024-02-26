e36f10d5ec09f5f322f84b411b23e5f44a5b1a49
Yosra Bahloul <ybahloul@diginov.tech>
2024-02-26 15:46:29 +0100

    add sitemap.ts file to robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
       disallow: '/private/',
-    }
+    },
+    sitemap: 'https://invoice-management-dashboard.vercel.app/sitemap.xml',
   }
 }
